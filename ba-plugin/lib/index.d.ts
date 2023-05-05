a0fa8d74d27c7a12b583dabad86c3c160fb0c248
Alin-a <65480503+Alin-sky@users.noreply.github.com>
2023-05-05 19:06:37 +0800

    Delete ba-plugin directory

--- ba-plugin/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Schema } from "koishi";
-export declare const name = "ba-plugin";
-export interface Config {
-}
-export declare const Config: Schema<Config>;
-export declare function apply(ctx: any): Promise<void>;
