c509ee5145a1a778e08c9d15a56af9d2cc51ba71
Alin-a <65480503+Alin-sky@users.noreply.github.com>
2023-05-05 19:08:35 +0800

    Add files via upload

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+import { Schema } from "koishi";
+export declare const jsx_runtime_1: any;
+export declare const name = "ba-plugin";
+export declare const usage = "\u6570\u636E\u6765\u6E90\u4E8Ebawiki\uFF0C\u66F4\u591A\u529F\u80FD\u6B63\u5728\u5F00\u53D1\u4E2D\uFF0C\u6709\u5565\u6BDB\u75C5\u53EF\u4EE5\u53BBGitHub\u4E0A\u63D0issue\uFF0C\u6216\u8005\u53D1\uD83D\uDCEB\u3002";
+export interface Config {
+}
+export declare const Config: Schema<Config>;
+export declare function apply(ctx: any): Promise<void>;
