f654044b686754c98072938cd1fd35c4b6888c47
Alin-a <ldl114514@gmail.com>
2023-05-12 19:05:13 +0800

    Delete lib directory

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Schema } from 'koishi';
-export declare const jsx_runtime_1: any;
-export declare const name = "ba-plugin";
-export declare const usage: string;
-export interface Config {
-}
-export declare const Config: Schema<Config>;
-export declare function apply(ctx: Context): void;
