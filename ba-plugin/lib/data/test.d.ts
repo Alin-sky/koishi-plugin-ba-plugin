53e0b2dbb03e0149ac5c4930cab32abd1378f68b
Alin-a <ldl114514@gmail.com>
2023-05-20 20:35:18 +0800

    Delete ba-plugin directory

--- ba-plugin/lib/data/test.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const jsx_runtime_1: any;
-export declare function 日服井(): string;
-export declare function 国际服井(): string;
-export declare function 日服十连(): string;
-export declare function 国际服十连(): string;
--- ba-plugin/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context, Schema } from 'koishi';
-export declare const name = "ba-plugin";
-export declare const usage: string;
-export interface Config {
-}
-export declare const Config: Schema<Config>;
-export declare function apply(ctx: Context): void;
