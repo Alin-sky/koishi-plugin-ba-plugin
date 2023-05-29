f8c499be8fee8f3a236f804a4e2d7e7a486c157a
Alin-a <ldl114514@gmail.com>
2023-05-29 23:59:38 +0800

    Delete ba-plugin directory

--- ba-plugin/lib/data/clairvoyant.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function accumulate(endTime: string, check: boolean, jjc: number, ability: number, rank: string): string[];
-export declare function getMessage(input: string): [string, boolean | null, number | null, number | null, string | null];
