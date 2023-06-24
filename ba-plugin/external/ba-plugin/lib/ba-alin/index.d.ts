eb9dd9199a16adcb8fc4cea8138cc154547f58a2
Alin-a <ldl114514@gmail.com>
2023-06-24 16:48:58 +0800

    Delete ba-plugin directory

--- ba-plugin/external/ba-plugin/lib/ba-alin/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from 'koishi';
-import { Config } from '..';
-export declare const alincloud = "http://124.221.99.85:8088/";
-export declare const alinplugin: {
-    apply(ctx: Context, config: Config): void;
-};
--- ba-plugin/lib/gacha/ps.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function gachaImageOne(_temporary: any, cardArray: any, stat: any, RPool: any, SRPool: any): Promise<Map<any, any>>;
-export declare function gachaImageTen(_temporary: any, cardArray: any, stat: any, RPool: any, SRPool: any): Promise<Map<any, any>>;
