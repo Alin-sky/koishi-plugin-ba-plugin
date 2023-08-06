f592e65a9aaec8a7db4e43608e58e07021ebc109
Alin-a <ldl114514@gmail.com>
2023-08-06 16:24:16 +0800

    Delete ba-plugin directory

--- ba-plugin/lib/gacha/data.d.ts
+++
@@ -1,9 +0,0 @@
-declare class stu {
-    name: string;
-    rare: number;
-    limit: number;
-    server: number;
-    constructor(name: string, rare: number, limit: number, server: number);
-}
-export declare let StudentPool: stu[];
-export {};
--- ba-plugin/lib/gacha/ps.d.ts
+++
@@ -1,3 +0,0 @@
-export declare module gachaImage {
-    function result(cardArray: any, stat: any, RPool: any, SRPool: any): Promise<Map<any, any>>;
-}
