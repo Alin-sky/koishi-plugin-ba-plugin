036c7b5a23dca69187670c80e96023e87329b99a
Alin-a <ldl114514@gmail.com>
2023-09-20 23:20:13 +0800

    Delete ba-plugin directory

--- ba-plugin/lib/sanae-code/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context, Schema } from 'koishi';
-export declare const name = "haoganjisuan";
-export interface Config {
-}
-export declare const Config: Schema<Config>;
-export declare const haoganjisuan: (ctx: Context) => void;
-export declare const levcom: (ctx: Context) => void;
--- ba-plugin/lib/sanae-fight/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'koishi';
-import { Config } from '..';
-export declare var jaup: any;
-export declare var inup: any;
-export declare const sanaefight: {
-    apply(ctx: Context, config: Config): void;
-};
