a7a5c0c8171a6dd893003768b4832a0f4c2f06bc
Alin-a <ldl114514@gmail.com>
2024-04-21 22:47:36 +0800

    Delete ba-plugin directory

--- ba-plugin/lib/calculate/cal_favor.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "koishi";
-export declare const inject: {
-    required: string[];
-};
-export declare const using: string[];
-export declare const plugin_ass: string[];
-export declare function cal_favorable(ctx: Context): Promise<void>;
--- ba-plugin/readme.md
+++
@@ -1,5 +0,0 @@
-# koishi-plugin-ba-plugin
-
-[![npm](https://img.shields.io/npm/v/koishi-plugin-ba-plugin?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-ba-plugin)
-
-
