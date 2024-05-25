213bd5326316b9eb58ef4278b7c3934f3e4ab2a4
Alin-a <ldl114514@gmail.com>
2024-05-26 01:25:28 +0800

    Delete lib directory

--- lib/get-active/get_active.d.ts
+++
@@ -1,4 +0,0 @@
-import { Context } from 'koishi';
-import { Config } from '..';
-export declare const inject: string[];
-export declare function active_get(ctx: Context, config: Config): Promise<void>;
